acdd2574f2de128b1f44371c650556b64442cbb7
msuneth <msuneth@gmail.com>
2024-02-26 08:08:06 +0530

    quizzler-question show on ui

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -5,28 +5,30 @@ THEME_COLOR = "#375362"
 
 
 class QuizUI:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
         self.window = Tk()
-        # self.quiz_brain = quiz_brain
+        self.quiz = quiz_brain
         self.window.title("Quizzler")
         self.score = 0
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.score_label = Label(text="Score: 0", bg=THEME_COLOR,fg="white")
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
 
-        self.canvas = Canvas(width=300, height=250, highlightthickness=0,bg="white")
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0, bg="white")
         # self.question_text = self.quiz_brain.next_question()
-        self.canvas.create_text(150, 125, text="",font=("Arial", 20, "italic"),fill=THEME_COLOR)
-        self.canvas.grid(row=1, column=0, columnspan=2,pady=50)
-#
+        self.question_text = self.canvas.create_text(150, 125,width=280, text="", font=("Arial", 20, "italic"),
+                                                     fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        #
         right_image = PhotoImage(file="images/true.png")
-        self.right_button = Button(image=right_image, highlightthickness=0,command=self.right_button_clicked)
+        self.right_button = Button(image=right_image, highlightthickness=0, command=self.right_button_clicked)
         self.right_button.grid(row=2, column=0)
 
         wrong_image = PhotoImage(file="images/false.png")
-        self.wrong_button = Button(image=wrong_image, highlightthickness=0,command=self.wrong_button_clicked)
+        self.wrong_button = Button(image=wrong_image, highlightthickness=0, command=self.wrong_button_clicked)
         self.wrong_button.grid(row=2, column=1)
+        self.show_next_question()
 
         self.window.mainloop()
 
@@ -35,7 +37,11 @@ class QuizUI:
 
     def wrong_button_clicked(self):
         pass
-#
+
+    def show_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
 # #
 # # flip_timer = window.after(100, display_random_word)
 #
