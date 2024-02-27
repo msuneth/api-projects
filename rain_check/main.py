1e58c81909ff4502f9d374eba6bcc4523a2e6b66
msuneth <msuneth@gmail.com>
2024-02-27 05:48:00 +0530

    rain check-twilio sms integration

--- rain_check/main.py
+++ rain_check/main.py
@@ -1,11 +1,14 @@
 import requests
+import os
+from twilio.rest import Client
 
-# API_KEY = "d8b5e5a49aa4c6eb71632c7df7e5a0a8"
-# MY_LAT =
-# MY_LONG =
+account_sid = os.environ.get("account_sid")
+print(account_sid)
+auth_token = os.environ.get("auth_token")
+appid = os.environ.get("appid")
 
 parameters = {
-    "appid": "d8b5e5a49aa4c6eb71632c7df7e5a0a8",
+    "appid": appid,
     "lat": 7.383017,
     "lon": 79.845523,
     "cnt": 4,
@@ -18,10 +21,20 @@ data = response.json()
 is_rain = False
 for item in data['list']:
     print(item['weather'][0]['id'])
-    if item['weather'][0]['id'] < 700:
+    if item['weather'][0]['id'] < 900:
         is_rain = True
 if is_rain:
     print("Bring umbrella")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body="Bring umbrella",
+        from_='+19309662195',
+        to='+94770675528'
+    )
+
+    print(message.sid)
 
 
 #https://api.openweathermap.org/data/2.5/forecast?lat=79.8352767&lon=7.3706207&appid=d8b5e5a49aa4c6eb71632c7df7e5a0a8
