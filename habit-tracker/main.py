a8cb17f9494b4ddc3c783cde1184e82efdfeb0f6
msuneth <msuneth@gmail.com>
2024-03-02 06:00:55 +0530

    habit tracker-update a pixel

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -20,12 +20,26 @@ graph_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs"
 headers = {
     "X-USER-TOKEN": pixela_token
 }
+graph_name = "code-graph"
 graph_config = {
     "id":"code-graph",
-    "name":"code-graph",
+    "name": graph_name,
     "unit":"hours",
     "type":"int",
     "color":"shibafu"
 }
 graph_response = requests.post(url=graph_endpoint,headers=headers,json=graph_config)
+print(graph_response.text)
+
+graph_name = "code-graph"
+graph_pixel_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{graph_name}"
+headers = {
+    "X-USER-TOKEN": pixela_token
+}
+
+graph_pixel_config = {
+    "date": "20240221",
+    "quantity": "5",
+}
+graph_response = requests.post(url=graph_pixel_endpoint,headers=headers,json=graph_pixel_config)
 print(graph_response.text)
