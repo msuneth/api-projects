d1d4066b9ceaabf4db367fd5955dced8218856b3
msuneth <msuneth@gmail.com>
2024-03-03 16:58:51 +0530

    habit tracker-pixel update to function

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,11 +1,10 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 with open('../config.json') as f:
     config_data = json.load(f)
 
-# Access database configuration
 pixela_token = config_data["pixela"]["token"]
 pixela_username = config_data["pixela"]["username"]
 user_info = {"token": pixela_token,
@@ -18,6 +17,7 @@ response = requests.post(url="https://pixe.la/v1/users", json=user_info)
 print(response.text)
 
 graph_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs"
+print(graph_endpoint)
 headers = {
     "X-USER-TOKEN": pixela_token
 }
@@ -31,18 +31,28 @@ graph_config = {
 }
 graph_response = requests.post(url=graph_endpoint, headers=headers, json=graph_config)
 print(graph_response.text)
-
-graph_name = "code-graph"
-graph_pixel_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{graph_name}"
 headers = {
     "X-USER-TOKEN": pixela_token
 }
 
+
+def graph_pixel_update(day: str):
+    global graph_name
+    graph_name = "code-graph"
+    graph_pixel_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{graph_name}"
+    print()
+    print(day)
+    graph_pixel_config = {
+        "date": day,
+        "quantity": "5",
+    }
+    graph_pixel_response = requests.post(url=graph_pixel_endpoint, headers=headers, json=graph_pixel_config)
+    print(graph_pixel_response.text)
+
+
 today = datetime.now().strftime("%Y%m%d")
-print(today)
-graph_pixel_config = {
-    "date": today,
-    "quantity": "5",
-}
-graph_response = requests.post(url=graph_pixel_endpoint, headers=headers, json=graph_pixel_config)
-print(graph_response.text)
+graph_pixel_update(today)
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+graph_pixel_update(yesterday)
+
+# update yesterday
