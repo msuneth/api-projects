1526b1d7ff3bfd01333bd120709b32652130a7c7
msuneth <msuneth@gmail.com>
2024-03-03 17:17:04 +0530

    habit tracker-pixel add pixel to function

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 with open('../config.json') as f:
     config_data = json.load(f)
+GRAPH_NAME = "code-graph"
 
 pixela_token = config_data["pixela"]["token"]
 pixela_username = config_data["pixela"]["username"]
@@ -21,10 +22,9 @@ print(graph_endpoint)
 headers = {
     "X-USER-TOKEN": pixela_token
 }
-graph_name = "code-graph"
 graph_config = {
     "id": "code-graph",
-    "name": graph_name,
+    "name": GRAPH_NAME,
     "unit": "hours",
     "type": "int",
     "color": "shibafu"
@@ -36,23 +36,28 @@ headers = {
 }
 
 
-def graph_pixel_update(day: str):
-    global graph_name
-    graph_name = "code-graph"
-    graph_pixel_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{graph_name}"
-    print()
-    print(day)
+def graph_pixel_add(day: str,qty: int):
+    add_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{GRAPH_NAME}"
     graph_pixel_config = {
         "date": day,
-        "quantity": "5",
+        "quantity": str(qty),
     }
     graph_pixel_response = requests.post(url=graph_pixel_endpoint, headers=headers, json=graph_pixel_config)
     print(graph_pixel_response.text)
 
 
-today = datetime.now().strftime("%Y%m%d")
-graph_pixel_update(today)
-yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-graph_pixel_update(yesterday)
+def graph_pixel_update(day: str,qty: int):
+    update_endpoint = f"https://pixe.la/v1/users/{pixela_username}/graphs/{GRAPH_NAME}/{day}"
+    graph_pixel_config = {
+        "quantity": str(qty),
+    }
+    graph_pixel_response = requests.put(url=graph_pixel_endpoint, headers=headers, json=graph_pixel_config)
+    print(graph_pixel_response.text)
 
-# update yesterday
+
+today = datetime.now().strftime("%Y%m%d")
+graph_pixel_add(today,5)
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+graph_pixel_add(yesterday,10)
+graph_pixel_update(yesterday,2)
+
