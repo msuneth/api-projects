62309c900b14fe2d0133c18bdb5433e598d4fff8
msuneth <msuneth@gmail.com>
2024-03-07 21:36:23 +0530

    flight-deals-data manager-read excel

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -1,3 +1,58 @@
+import requests
+import json
+
+
+# from datetime import datetime, timedelta
+#
+
+#
+# nutritionix_apikey = config_data["nutritionix"]["API_KEY"]
+# nutritionix_appid = config_data["nutritionix"]["APP_ID"]
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        with open('../config.json') as f:
+            config_data = json.load(f)
+
+        # call google sheets
+        self.sheet_bearer_token = config_data["flight_deals"]["sheets_info"]["bearer_token"]
+        self.sheet_apikey = config_data["flight_deals"]["sheets_info"]["api_key"]
+        self.auth_header = {
+            "Authorization": self.sheet_bearer_token
+        }
+
+    def read_excel(self):
+        sheet_response = requests.get(url=f"https://api.sheety.co/{self.sheet_apikey}/flightDeals2024/prices"
+                                       , headers=self.auth_header)
+        return sheet_response
+
+    def write_excel(self):
+        pass
+        # data = {"price": {
+        #     "date": today,
+        #     "time": time_now,
+        #     "exercise": workout_name,
+        #     "duration": workout_duration,
+        #     "calories": workout_calories
+        # }}
+        #
+        # sheet_response = requests.post(url=f"https://api.sheety.co/{sheet_apikey}/myWorkouts2024/workouts"
+        #                                , headers=auth_header, json=data)
+        # print(sheet_response.text)
+
+    def update_excel(self):
+        pass
+
+    #
+    # workouts = response_data["exercises"]
+    # for workout in workouts:
+    #     workout_name = workout["name"].title()
+    #     workout_duration = workout["duration_min"]
+    #     workout_calories = workout["nf_calories"]
+    #     print(response_data)
+    #     today = datetime.now().strftime("%d/%m/%Y")
+    #     print(today)
+    #     time_now = datetime.now().strftime("%H:%M:%S")
+    #     print(time_now)
+    #
--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,1 +1,10 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+
+data_manager = DataManager()
+print(data_manager.read_excel().json())
+
+
+
