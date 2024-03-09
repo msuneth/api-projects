fa2867732bd85808b82b45b2f34ebd22613aecd0
msuneth <msuneth@gmail.com>
2024-03-09 22:20:45 +0530

    flight-deals-flight search-load json data

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -1,3 +1,10 @@
+import json
+import requests
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        with open('../config.json') as f:
+            config_data = json.load(f)
+        self.flight_search_api_token = config_data["flight-search"]["host"]
+
