12e3f8efc62b2d8edf76612bed1c288bbeb329d0
msuneth <msuneth@gmail.com>
2024-03-10 17:33:19 +0530

    flight-deals-data manager-get iata by city name

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -1,12 +1,23 @@
 import json
 import requests
 
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
     def __init__(self):
         with open('../config.json') as f:
             config_data = json.load(f)
-        self.flight_search_api_token = config_data["flight-search"]["host"]
+        self.flight_search_api_key = config_data["flight_deals"]["flight-search"]["api_key"]
 
-    def search_iata_by_city(self,city):
-        pass
+    def search_iata_by_city(self, city):
+        url = "https://api.tequila.kiwi.com/locations/query"
+        parameters = {
+            "term": city,
+            "location_types": "city"
+        }
+        headers = {
+            "accept": "application/json",
+            "apikey": self.flight_search_api_key
+        }
+        response = requests.get(url=url, headers=headers, params=parameters)
+        return response.json()["locations"][0]["code"]
--- flight-deals/main.py
+++ flight-deals/main.py
@@ -14,10 +14,13 @@ data = {"price": {
     "iataCode": "Test",
     "lowestPrice": "1000"
 }}
+
+flight_info = FlightSearch()
+print(flight_info.search_iata_by_city("Colombo"))
 # print(data_manager.write_excel(data).json())
 # print(data_manager.update_excel(data,11).json())
-notifications = NotificationManager()
-#notifications.send_sms("+94770675528","Test SMS")
-notifications.send_email("msuneth@gmail.com","Subject:Test\n\nTest")
+# notifications = NotificationManager()
+# #notifications.send_sms("+94770675528","Test SMS")
+# notifications.send_email("msuneth@gmail.com","Subject:Test\n\nTest")
 
 
